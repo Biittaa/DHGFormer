f55fa5756b412d495d0f572f1f92523eb237f60c
Biittaa <123160799+Biittaa@users.noreply.github.com>
2026-09-18 12:43:42 +0330

    Disable Mixed data for smri

--- util/prepossess.py
+++ util/prepossess.py
@@ -36,7 +36,7 @@ def mixup_data(x, nodes, y, alpha=1.0, device='cuda', extra=None):
 
     if extra is not None:
         # mixed_extra = lam * extra + (1 - lam) * extra[index, :]
-        return mixed_x, mixed_nodes, y_a, y_b, lam, mixed_extra
+        return mixed_x, mixed_nodes, y_a, y_b, lam, extra
 
     return mixed_x, mixed_nodes, y_a, y_b, lam
 
