fad355ce3715304dbd462284066efebcc15cbb0f
Biittaa <123160799+Biittaa@users.noreply.github.com>
2026-09-20 23:09:41 +0330

    Resolve indentation issue in Python file

--- model/MultiViewGCN.py
+++ model/MultiViewGCN.py
@@ -370,7 +370,7 @@ class MultiViewGCN(nn.Module):
                     h = self.att_pool[view](h, batch_vec)
             else:
                 h = tg.nn.global_mean_pool(h, batch_vec)
-            view_embeddings.append(h)
+                view_embeddings.append(h)
             fused = fuse_view_embeddings(self.fusion_type, self.fusion_module, view_embeddings)
             if self.extra_dim > 0 and extra is not None:
                 fused = torch.cat([fused, self.extra_mlp(extra)], dim=1)
